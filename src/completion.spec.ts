35f7ec8e9e9f04c4438b861b508918deb1b012f2
Jeff Principe <princjef@gmail.com>
2017-06-09 21:47:48 -0700

    test: add tests for completion

--- src/completion.spec.ts
+++ src/completion.spec.ts
@@ -1,35 +1,182 @@
-import test from 'ava';
+import * as ava from 'ava';
+import * as chalk from 'chalk';
 
-test.todo('filters the options based on the last argument');
+import Completion from './completion';
 
-test.todo('performs no filter when the last argument matches none of the arguments');
+function contextualize<T>(getContext: () => T): ava.RegisterContextual<T> {
+    ava.test.beforeEach(t => {
+        Object.assign(t.context, getContext());
+    });
 
-test.todo('selects nothing when multiple valid options are present');
+    return ava.test;
+}
 
-test.todo('selects the only valid option when provided');
+const test = contextualize(() => ({
+    completions: [
+        'and',
+        'form',
+        'any',
+        'other',
+        'multiple words'
+    ]
+}));
 
-test.todo('selectNext(): selects the first completion when none are selected');
+test('filters the options based on the last argument', t => {
+    const completion = Completion.populate(['one', 'a'], t.context.completions);
+    t.is(completion.possibleCompletionCount, 2);
+    t.is(completion.selection, null);
+});
 
-test.todo('selectNext(): selects the next completion when a completion other than the last is selected');
+test('performs no filter when the last argument matches none of the arguments', t => {
+    const completion = Completion.populate(['some', 'goobledygook'], t.context.completions);
+    t.is(completion.possibleCompletionCount, t.context.completions.length);
+    t.is(completion.selection, null);
+});
 
-test.todo('selectNext(): selects the first completion when the last completion is selected');
+test('selects nothing when multiple valid options are present', t => {
+    const completion = Completion.populate([''], t.context.completions);
+    t.is(completion.possibleCompletionCount, t.context.completions.length);
+    t.is(completion.selection, null);
+});
 
-test.todo('selectPrevious(): selects the last completion when none are selected');
+test('selects the only valid option when provided', t => {
+    const completion = Completion.populate(['oth'], t.context.completions);
+    t.is(completion.possibleCompletionCount, 1);
+    t.is(completion.selection, 'other');
+});
 
-test.todo('selectPrevious(): selects the previous completion when a completion other than the first is selected');
+test('selectNext(): selects the first completion when none are selected', t => {
+    const completion = Completion.populate([''], t.context.completions);
+    t.is(completion.selection, null);
 
-test.todo('selectPrevious(): selects the last completion when the first completion is selected');
+    completion.selectNext();
+    t.is(completion.selection, t.context.completions[0]);
+});
 
-test.todo('renderCommand(): shows the provided command if no selections are chosen');
+test('selectNext(): selects the next completion when a completion other than the last is selected', t => {
+    const completion = Completion.populate(['a'], t.context.completions);
+    t.is(completion.selection, null);
 
-test.todo('renderCommand(): fills in the last argument with the given selection when it matches');
+    completion.selectNext();
+    t.is(completion.selection, 'and');
 
-test.todo('renderCommand(): adds a space to the end of the command if the last argument doesn\'t match any selections and none are selected');
+    completion.selectNext();
+    t.is(completion.selection, 'any');
+});
 
-test.todo('renderCommand(): ensures a space is present and shows the selection at the end if it doesn\'t match the last argument');
+test('selectNext(): selects the first completion when the last completion is selected', t => {
+    const completion = Completion.populate(['a'], t.context.completions);
+    t.is(completion.selection, null);
 
-test.todo('renderCommand(): adds the selection to the end of the command if the command ends in whitespace');
+    completion.selectNext();
+    t.is(completion.selection, 'and');
 
-test.todo('renderOptions(): returns all options mernalized to the length of the longest option');
+    completion.selectNext();
+    t.is(completion.selection, 'any');
 
-test.todo('renderOptions(): inverts the colors on the current selection when present');
+    completion.selectNext();
+    t.is(completion.selection, 'and');
+});
+
+test('selectPrevious(): selects the last completion when none are selected', t => {
+    const completion = Completion.populate([''], t.context.completions);
+    t.is(completion.selection, null);
+
+    completion.selectPrevious();
+    t.is(completion.selection, t.context.completions[t.context.completions.length - 1]);
+});
+
+test('selectPrevious(): selects the previous completion when a completion other than the first is selected', t => {
+    const completion = Completion.populate([''], t.context.completions);
+    t.is(completion.selection, null);
+
+    completion.selectPrevious();
+    t.is(completion.selection, t.context.completions[t.context.completions.length - 1]);
+
+    completion.selectPrevious();
+    t.is(completion.selection, t.context.completions[t.context.completions.length - 2]);
+});
+
+test('selectPrevious(): selects the last completion when the first completion is selected', t => {
+    const completion = Completion.populate(['a'], t.context.completions);
+    t.is(completion.selection, null);
+
+    completion.selectPrevious();
+    t.is(completion.selection, 'any');
+
+    completion.selectPrevious();
+    t.is(completion.selection, 'and');
+
+    completion.selectPrevious();
+    t.is(completion.selection, 'any');
+});
+
+test('renderCommand(): shows the provided command if no selections are chosen', t => {
+    const completion = Completion.populate([''], t.context.completions);
+    t.is(completion.selection, null);
+
+    const command = 'some command';
+    t.is(completion.renderCommand(command), command);
+});
+
+test('renderCommand(): fills in the last argument with the given selection when it matches', t => {
+    const completion = Completion.populate(['some', 'ot'], t.context.completions);
+    t.is(completion.selection, 'other');
+
+    const command = 'some ot';
+    t.is(completion.renderCommand(command), 'some other');
+});
+
+test('renderCommand(): ensures a space is present and shows the selection at the end if it doesn\'t match the last argument', t => {
+    const completion = Completion.populate(['some', 'gobbledygook'], t.context.completions);
+    t.is(completion.selection, null);
+
+    completion.selectNext();
+    t.is(completion.selection, t.context.completions[0]);
+
+    const command = 'some goobledygook';
+    t.is(completion.renderCommand(command), `${command} ${t.context.completions[0]}`);
+});
+
+test('renderCommand(): adds the selection to the end of the command if the command ends in whitespace', t => {
+    const completion = Completion.populate(['some', 'gobbledygook'], t.context.completions);
+    t.is(completion.selection, null);
+
+    completion.selectNext();
+    t.is(completion.selection, t.context.completions[0]);
+
+    const command = 'some goobledygook ';
+    t.is(completion.renderCommand(command), `${command}${t.context.completions[0]}`);
+});
+
+test('renderOptions(): returns all options normalized to the length of the longest option', t => {
+    const options = [
+        'short',
+        'superlongggggg',
+        'two words'
+    ];
+    const completion = Completion.populate([''], options);
+
+    t.deepEqual(completion.renderOptions(), [
+        'short         ',
+        'superlongggggg',
+        'two words     '
+    ]);
+});
+
+test('renderOptions(): inverts the colors on the current selection when present', t => {
+    const options = [
+        'short',
+        'superlongggggg',
+        'two words'
+    ];
+    const completion = Completion.populate([''], options);
+    completion.selectNext();
+    t.is(completion.selection, 'short');
+
+    t.deepEqual(completion.renderOptions(), [
+        chalk.inverse('short         '),
+        'superlongggggg',
+        'two words     '
+    ]);
+});
